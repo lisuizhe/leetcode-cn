0d0f2ce8f492c003a2c274ffcfa0d5071a1f341b
suizhe-li <suizhe.li@woven-planet.global>
2022-08-28 15:40:44 +0900

    121.买卖股票的最佳时机

---
+++ go/121.买卖股票的最佳时机.go
@@ -0,0 +1,48 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=121 lang=golang
+ *
+ * [121] 买卖股票的最佳时机
+ */
+
+// @lc code=start
+
+// i: day, j: max sold times
+
+// dp:
+// dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+// dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
+
+// base case:
+// dp[-1][j][0] = dp[i][0][0] = 0
+// dp[-1][j][1] = dp[i][0][1] = -Inf
+func maxProfit(prices []int) int {
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+
+	dp := make([][2]int, n)
+	for i := 0; i < n; i++ {
+		var dp0, dp1 int
+		if i == 0 {
+			dp0 = 0
+			dp1 = -prices[i]
+		} else {
+			dp0 = max(dp[i-1][0], dp[i-1][1]+prices[i])
+			dp1 = max(dp[i-1][1], -prices[i])
+		}
+		dp[i] = [2]int{dp0, dp1}
+	}
+	return dp[n-1][0]
+}
+
+func max(a, b int) int {
+	if b > a {
+		return b
+	}
+	return a
+}
+
+// @lc code=end
