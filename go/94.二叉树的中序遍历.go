0ee071be65592c604275ba0248575e46a5f35cd4
suizhe-li <suizhe.li@woven-planet.global>
2022-09-18 08:13:32 +0900

    94.二叉树的中序遍历

---
+++ go/94.二叉树的中序遍历.go
@@ -0,0 +1,29 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=94 lang=golang
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+//
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	res := []int{}
+	if root == nil {
+		return res
+	}
+	res = append(res, inorderTraversal(root.Left)...)
+	res = append(res, root.Val)
+	res = append(res, inorderTraversal(root.Right)...)
+	return res
+}
+
+// @lc code=end
